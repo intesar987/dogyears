f1ee325a66a0ec66c2c702c9f66bc41533d0a653
INTESAR NUR <51456550+intesar987@users.noreply.github.com>
2024-02-13 00:01:45 +0000

    Create dogyears.js
    shows how to convert your age to dog years

---
+++ dogyears.js
@@ -0,0 +1,19 @@
+let myAge = 4;
+//age of human
+let earlyYears = 2
+// first 2 years of dog life
+
+earlyYears *= 10.5
+
+laterYears = myAge - 2
+//earlyyears of a dog is 2 x 10.5. Later years accounts for the years after the first 2 dog years
+
+laterYears *= 4
+//each human year is 4 dog years
+
+myAgeInDogYears = earlyYears + laterYears;
+//adding the first 2 years of a dog life and latter years.
+
+let myName ='Intesar'.toLowerCase() 
+//lowecase all letters of my name
+console.log(`My name is ${myName}.  I am ${myAge} years old in humans years which is ${myAgeInDogYears} years old in dog years`)
